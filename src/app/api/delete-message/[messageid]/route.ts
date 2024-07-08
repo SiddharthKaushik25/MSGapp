e58ff1ea0545cbedce9505a5c9a4d77388961d52
SiddharthKaushik25 <siddharthkaushik25@gmail.com>
2024-07-08 20:54:13 +0530

    add delete message api

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -18,5 +18,37 @@ export async function DELETE(req: Request, {params}:{params:{messageid: string}}
       },
       { status: 400 }
     );
+  
+  }
+  try {
+    const result = await UserModel.updateOne(
+      {_id: user._id,}, 
+      {$pull: {messages: {messageid: messageId}}}
+    )
+    if(result.modifiedCount ==0){
+      return Response.json(
+        {
+          success: false,
+          message: "Message not found or already deleted",
+        },
+        { status: 404 }
+      );
+    }
+    return Response.json(
+      {
+        success: true,
+        message: "Message deleted",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error)
+    return Response.json(
+      {
+        success: false,
+        message: "Something went Wrong",
+      },
+      { status: 500 }
+    );
   }
 }
